9fde833f601b8b00951c2a624bd18b219974710e
yjhmelody <465402634@qq.com>
2018-09-06 22:54:14 +0800

    chp3: finished

---
+++ ch3.rkt
@@ -0,0 +1,30 @@
+#lang plai-typed
+
+(define-type ArithC
+  [numC (n : number)]
+  [plusC (l : ArithC) (r : ArithC)]
+  [multC (l : ArithC) (r : ArithC)])
+
+(define (parse [s : s-expression])
+  (cond
+    [(s-exp-number? s) (numC (s-exp->number s))]
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)])
+       (case (s-exp->symbol (first sl))
+         [(+) (plusC (parse (second sl)) (parse (third sl)))]
+         [(*) (multC (parse (second sl)) (parse (third sl)))]
+         [else (error 'parse "invalid list input")]))]
+    [else (error 'parse "invalid input")]))
+
+(define (interp [a : ArithC]) : number
+  (type-case ArithC a
+    [numC (n) n]
+    [plusC (l r) (+ (interp l) (interp r))]
+    [multC (l r) (* (interp l) (interp r))]))
+
+
+(define case1 '(+ 1 (* 2 3)))
+(define case2 '(* 2 (+ 3 4)))
+
+(define res1 (interp (parse case1)))
+(define res2 (interp (parse case2)))
