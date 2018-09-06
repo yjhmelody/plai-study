c658696ad55888aec8764b157018108e695b0ea1
yjhmelody <465402634@qq.com>
2018-09-06 22:25:50 +0800

    ch2: finished

---
+++ ch1.rkt
@@ -0,0 +1,34 @@
+#lang plai-typed
+
+; ADT
+(define-type MisspelledAnimal
+  [caml (humps : number)]
+  [yacc (height : number)])
+
+(define a (caml 1))
+(define b (yacc 2))
+
+(define c : MisspelledAnimal (caml 1))
+(define d : MisspelledAnimal (yacc 2))
+
+; type match
+(define (good? [ma : MisspelledAnimal]) : boolean
+  (type-case MisspelledAnimal ma [caml (humps) (>= humps 2)] [yacc (height) (> height 2.1)]))
+
+; 很少有需要用到类型判断函数(如caml?),不过可以用。
+; 数据类型定义时还会生成字段提取函数,例如caml-humps。
+
+(define (foo [a : number] [b : number]) : number (+ a b))
+
+; tuple
+(define tuple (values 1 2 3))
+(define-values (t1 t2 t3) tuple)
+(define-values ([t4 : number] [t5 : number] [t6 : number]) tuple)
+
+(define curry2 (lambda (f) (lambda (a) (lambda (b) (f a b)))))
+(define curry3 (lambda (f) (lambda (a) (lambda (b) (lambda (c) (f a b c))))))
+
+(define-type ArithC
+  [numC (n : number)]
+  [plusC (+ (l : ArithC) (r : ArithC))]
+  [multC (* (l : ArithC) (r : ArithC))])
---
+++ ch2.rkt
@@ -0,0 +1,21 @@
+#lang plai-typed
+
+(define l '(+ 1 2))
+
+(define f (first (s-exp->list l)))
+
+(define-type ArithC
+  [numC (n : number)]
+  [plusC (l : ArithC) (r : ArithC)]
+  [multC (l : ArithC) (r : ArithC)])
+
+(define (parse [s : s-expression])
+  (cond
+    [(s-exp-number? s) (numC (s-exp->number s))]
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)])
+       (case (s-exp->symbol (first sl))
+         [(+) (plusC (parse (second sl)) (parse (third sl)))]
+         [(*) (multC (parse (second sl)) (parse (third sl)))]
+         [else (error 'parse "invalid list input")]))]
+    [else (error 'parse "invalid input")]))
