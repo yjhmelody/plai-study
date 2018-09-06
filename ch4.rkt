ab7004719eb3705c41e44b2c42b06bdae01685f7
yjhmelody <465402634@qq.com>
2018-09-06 23:22:09 +0800

    ch4: finished

---
+++ ch4.rkt
@@ -0,0 +1,44 @@
+#lang plai-typed
+
+; core
+(define-type ArithC
+  [numC (n : number)]
+  [plusC (l : ArithC) (r : ArithC)]
+  [multC (l : ArithC) (r : ArithC)])
+
+; extented
+(define-type ArithS
+  [numS (n : number)]
+  [plusS (l : ArithS) (r : ArithS)]
+  [bminusS (l : ArithS) (r : ArithS)]
+  [uminusS (e : ArithS)]
+  [multS (l : ArithS) (r : ArithS)])
+
+(define (parse [s : s-expression])
+  (cond
+    [(s-exp-number? s) (numC (s-exp->number s))]
+    [(s-exp-list? s)
+     (let ([sl (s-exp->list s)])
+       (case (s-exp->symbol (first sl))
+         [(+) (plusC (parse (second sl)) (parse (third sl)))]
+         [(*) (multC (parse (second sl)) (parse (third sl)))]
+         [else (error 'parse "invalid list input")]))]
+    [else (error 'parse "invalid input")]))
+
+(define (interp [a : ArithC]) : number
+  (type-case ArithC a
+    [numC (n) n]
+    [plusC (l r) (+ (interp l) (interp r))]
+    [multC (l r) (* (interp l) (interp r))]))
+
+; desugar ArithS to ArithC
+(define (desugar [as : ArithS]) : ArithC
+  (type-case ArithS as
+    [numS (n) (numC n)]
+    [plusS (l r) (plusC (desugar l) (desugar r))]
+    [multS (l r) (multC (desugar l) (desugar r))]
+    ; -b = -1 * b
+    [uminusS (e) (multC (numC -1) (desugar e))]
+    ; a - b = a + -1 × b
+    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
+))
