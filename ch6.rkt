55f0de1443f2dc51f87effbf371084e27cea3760
yjhmelody <465402634@qq.com>
2018-09-07 22:07:29 +0800

    ch6: refactor lookup

--- ch6.rkt
+++ ch6.rkt
@@ -53,18 +53,19 @@
 (define (lookup [name : symbol] [env : Env]) : number
 (cond 
     [(empty? env) (error 'lookup "name not found")]
-    [else (cond
-            [(symbol=? name (bind-name (first env))) (bind-val (first env))]
-            [else (lookup name (rest env))]
-    )]
+    [(symbol=? name (bind-name (first env))) (bind-val (first env))]
+    [else (lookup name (rest env))]
 ))
 
 (test (interp (plusC (numC 10) (appC 'const5 (numC 10)))
-    mt-env (list (fdC 'const5 '_ (numC 5)))) 15)
+    mt-env (list (fdC 'const5 '_ (numC 5)))) 
+    15)
 
 (test (interp (plusC (numC 10) (appC 'double (plusC (numC 1) (numC 2))))
-    mt-env (list (fdC 'double 'x (plusC (idC 'x) (idC 'x))))) 16)
+    mt-env (list (fdC 'double 'x (plusC (idC 'x) (idC 'x))))) 
+    16)
 
 (test (interp (plusC (numC 10) (appC 'quadruple (plusC (numC 1) (numC 2))))
-    mt-env (list (fdC 'quadruple 'x (appC 'double (appC 'double (idC 'x))))
-(fdC 'double 'x (plusC (idC 'x) (idC 'x))))) 22)
+    mt-env (list (fdC 'quadruple 'x (appC 'double (appC 'double (idC 'x)))) 
+        (fdC 'double 'x (plusC (idC 'x) (idC 'x))))) 
+    22)
